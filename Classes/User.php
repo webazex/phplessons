5d4f508c6b49f6205f4c02295d2ab3366b304748
webazex <webazex@gmail.com>
2024-11-09 18:36:55 +0200

    add skeleton methods for class User

--- Classes/User.php
+++ Classes/User.php
@@ -2,5 +2,16 @@
 
 class User
 {
+    private string $name;
+    private string $email;
+    private int $age;
 
+    public function __call(string $name, array $arguments)
+    {
+
+    }
+
+    public function getAll():array {
+
+    }
 }
