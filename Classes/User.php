c1bf3e5569b926038be2888f8e73410aa2bcc068
webazex <webazex@gmail.com>
2024-11-09 20:06:44 +0200

    added User class methods

--- Classes/User.php
+++ Classes/User.php
@@ -2,16 +2,41 @@
 
 class User
 {
-    private string $name;
-    private string $email;
-    private int $age;
+    private ?string $name = null;
+    private ?string $email = null;
+    private ?int $age = null;
+
+    public function __construct(string $name, string $email, int $age )
+    {
+        $this->age = $age;
+        $this->name = $name;
+        $this->email = $email;
+    }
 
     public function __call(string $name, array $arguments)
     {
-
+        try {
+            if (!method_exists($this, $name)) {
+                Throw new \Exception("Метод не існує");
+            }else{
+                return call_user_func_array([$this, $name], $arguments);
+            }
+        } catch (\Exception $e){
+            echo $e->getMessage();
+        }
     }
 
-    public function getAll():array {
+    public function getAll(): array
+    {
+        $result = [];
+        $reflection = new \ReflectionClass($this);
 
+        foreach ($reflection->getProperties() as $property) {
+            $property->setAccessible(true);
+            $value = $property->getName() === 'email' ? '' : $property->getValue($this); // поле email залишається порожнім
+            $result[$property->getName()] = $value;
+        }
+
+        return $result;
     }
 }
--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php
+require_once "Classes".DIRECTORY_SEPARATOR."User.php";
+$user = new User("Rendal", "w@mail.com", 30);
+var_dump($user->getAll());
