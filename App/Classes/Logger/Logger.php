d3d059afae8970328668cdbcb41410685e6bafb7
webazex <webazex@gmail.com>
2024-11-12 21:05:46 +0200

    add Logger class

---
+++ App/Classes/Logger/Logger.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ACMS\App\Classes\Logger;
+class Logger
+{
+    private string $logFile;
+
+    public function __construct(string $logFile = 'debug.log')
+    {
+        $this->logFile = $logFile;
+    }
+
+    public function log(string $level, string $message): void
+    {
+        $timestamp = date('Y-m-d H:i:s');
+        $logMessage = "[{$timestamp}] {$level}: {$message}" . PHP_EOL;
+
+        file_put_contents($this->logFile, $logMessage, FILE_APPEND);
+    }
+
+    public function info(string $message): void
+    {
+        $this->log('INFO', $message);
+    }
+
+    public function warning(string $message): void
+    {
+        $this->log('WARNING', $message);
+    }
+
+    public function error(string $message): void
+    {
+        $this->log('ERROR', $message);
+    }
+}
+
+// Пример использования
+$logger = new Logger();
+$logger->info('This is an info message');
+$logger->warning('This is a warning message');
+$logger->error('This is an error message');
